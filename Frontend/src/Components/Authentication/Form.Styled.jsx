b6629eda73d44698366759570778756faf4f8233
Raman Pandey <ramanpandey413@gmail.com>
2025-04-23 00:25:35 +0530

    Updated Readme

--- Frontend/src/Components/Authentication/Form.Styled.jsx
+++ Frontend/src/Components/Authentication/Form.Styled.jsx
@@ -1,13 +1,13 @@
 import styled from "styled-components";
 
-export const BigContainer=styled.div`
-background: #000000;
-    width:100vw;
-    height: 100vh;
-    display:flex;
-    justify-content: center;
-    align-items: center;
-`
+export const BigContainer = styled.div`
+  background: #000000;
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 export const PageContainer = styled.div`
   display: flex;
   flex-direction: row;
@@ -19,51 +19,53 @@ export const PageContainer = styled.div`
 `;
 
 export const FormContainer = styled.form`
-padding: 10px;
-    margin: auto;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    height: 70vh;
-    width: 30vw;
-    background: #171717;
-    `
+  padding: 10px;
+  margin: auto;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 70vh;
+  width: 30vw;
+  background: #171717;
+`;
 export const ImageContainer1 = styled.div`
   width: 35vw;
   height: 70vh;
-  background: url('https://images.pexels.com/photos/841130/pexels-photo-841130.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2') no-repeat center center/cover;
+  background: url("https://images.pexels.com/photos/841130/pexels-photo-841130.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2")
+    no-repeat center center/cover;
 `;
 export const ImageContainer2 = styled.div`
   width: 35vw;
   height: 70vh;
-  background: url('https://e0.pxfuel.com/wallpapers/777/418/desktop-wallpaper-womens-fitness-female-fitness.jpg') no-repeat center center/cover;
+  background: url("https://e0.pxfuel.com/wallpapers/777/418/desktop-wallpaper-womens-fitness-female-fitness.jpg")
+    no-repeat center center/cover;
   background-size: cover;
 `;
 
-export const StyledInputField=styled.input`
-border: none;
-margin-top: 9%;
-color:#FFFFFF;
-&:focus {
+export const StyledInputField = styled.input`
+  border: none;
+  margin-top: 9%;
+  color: #ffffff;
+  &:focus {
     outline: none;
     border-color: transparent;
     box-shadow: 0 0 0 2px #f97316;
   }
 
   transition: all 0.3s ease;
-background: #262626;
-outline: 0.5px solid #404040;
-padding:15px;
-height: 5vh;
-width:40vh;
-::placeholder{
-  color: #FFFFFF;
-}
-`
+  background: #262626;
+  outline: 0.5px solid #404040;
+  padding: 15px;
+  height: 5vh;
+  width: 40vh;
+  ::placeholder {
+    color: #ffffff;
+  }
+`;
 
 export const StyledButton = styled.button`
-text-align: center;
+  text-align: center;
   border: none;
   margin-top: 10%;
   padding: 15px;
@@ -71,26 +73,25 @@ text-align: center;
   width: 40vh;
   cursor: pointer;
   transition: background-color 0.3s ease, background-image 0.3s ease;
-  background: linear-gradient(to right, #EA5A0C, #F59D0B);
+  background: linear-gradient(to right, #ea5a0c, #f59d0b);
   box-shadow: 0 10px 15px -3px rgba(234, 88, 12, 0.2);
-  color: #FFFFFF;
+  color: #ffffff;
 
   &:hover {
     background-image: linear-gradient(to right, #f97316, #fbbf24);
   }
 `;
 
-export const StyledText=styled.p`
-margin-top: 6%;
-color: white;
-text-decoration: none;
-`
-export const StyledSpan=styled.span`
-
-text-decoration: none;
-color: #F97316;
-&:visited {
-    color: #F97316;
+export const StyledText = styled.p`
+  margin-top: 6%;
+  color: white;
+  text-decoration: none;
+`;
+export const StyledSpan = styled.span`
+  text-decoration: none;
+  color: #f97316;
+  &:visited {
+    color: #f97316;
     text-decoration: none;
   }
-`
+`;
--- Frontend/src/Components/Dashboard/Header.jsx
+++ Frontend/src/Components/Dashboard/Header.jsx
@@ -1,17 +1,25 @@
-import React from 'react'
-import logo from '../../assets/final.png'
-import { StyledDiv, StyledPara, StyledHeader, StyledHeaderWrapper } from './StyledDashboard'
+import React from "react";
+import logo from "../../assets/final.png";
+import {
+  StyledDiv,
+  StyledPara,
+  StyledHeader,
+  StyledHeaderWrapper,
+} from "./StyledDashboard";
 
 const Header = () => {
-  return (<StyledHeaderWrapper> 
-  
-    <img src={logo} style={{height:"7%",width:"4%",marginLeft:"0%",marginTop:"0%"}}></img>
-    <StyledDiv>
+  return (
+    <StyledHeaderWrapper>
+      <img
+        src={logo}
+        style={{ height: "7%", width: "4%", marginLeft: "0%", marginTop: "0%" }}
+      ></img>
+      <StyledDiv>
         <StyledHeader>Welcome back </StyledHeader>
         <StyledPara>Ready to achieve your fitness goals today?</StyledPara>
-    </StyledDiv>
+      </StyledDiv>
     </StyledHeaderWrapper>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
